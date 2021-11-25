fe68ae598f28067d3f28791c4b1ecfc312f2ca30
quimpm <quimpm99@gmail.com>
2021-11-25 23:13:37 +0100

    Finished Dataproducer2 BOYYYZZZ

--- examples/dataproducer2/dataproducer2.ino
+++ examples/dataproducer2/dataproducer2.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal_I2C.h>
+
 /* Dataproducer 2
  * 
  * 
@@ -8,25 +10,68 @@
 #include <Adafruit_ADXL345_U.h>
 
 #include <Wire.h>      // libreria de comunicacion por I2C
-#include <LCD.h>      // libreria para funciones de LCD
-#include <LiquidCrystal_I2C.h>    // libreria para LCD por I2C
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "LowiA37F";
+const char* password = "33XUAYNAKKPXFM";
+const char* mqtt_server = "192.168.0.29";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
 
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7); // DIR, E, RW, RS, D4, D5, D6, D7
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // DIR, E, RW, RS, D4, D5, D6, D7
+
+void setup_wifi() {
+
+  delay(10);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  randomSeed(micros());
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      client.publish("debug","connected");
+      client.publish("outTopic", "hello world");
+    } else {
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
-  Serial.begin(9600);
-  lcd.setBacklightPin(3, POSITIVE); // puerto P3 de PCF8574 como positivo
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.publish("debug", "Setting LCD communication");
   lcd.setBacklight(HIGH);   // habilita iluminacion posterior de LCD
-  lcd.begin(16, 2);     // 16 columnas por 2 lineas para LCD 1602A
+  lcd.begin(0, 2);     // 16 columnas por 2 lineas para LCD 1602A
   lcd.clear();      // limpia pantalla
-  Serial.println("A");
   if (!accel.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    client.publish("debug", "Ooops, no ADXL345 detected ... Check your wiring!");
     while (1);
   }
   accel.setRange(ADXL345_RANGE_16_G);
@@ -35,13 +80,14 @@ void setup()
 
 void loop()
 {
-  Serial.println("B");
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
   sensors_event_t event;
   accel.getEvent(&event);
-  Serial.println("Displaying information: ");
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  "); Serial.println("m/s^2 ");
+  client.publish("debug", "Displaying information at LCD SCReen: ");
   lcd.setCursor(0, 0);    // ubica cursor en columna 0 y linea 0
   lcd.print("X:");
   lcd.print(event.acceleration.x);
@@ -50,5 +96,14 @@ void loop()
   lcd.setCursor(0, 1);
   lcd.print("Z: ");
   lcd.print(event.acceleration.z);// ubica cursor en columna 0 y linea 1
-  delay(500);
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    sprintf(msg, "{\"X\":%f,\"Y\":%f,\"Z\":%f}", event.acceleration.x, event.acceleration.y, event.acceleration.z);
+    client.publish("debug", "Published message");
+    client.publish("debug","Publish message: ");
+    client.publish("debug",msg);
+    client.publish("windmill/dataproducer2", msg);
+  }
+  client.publish("debug", "Finished Message Sending");
+  delay(2500);
 }
